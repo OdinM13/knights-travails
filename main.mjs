14fb104e8ccb02f66501c0713f4d006cc713a526
OdinM13 <markodinius@gmail.com>
2026-05-04 08:33:39 +0200

    WIP: basic structure of knightmoves

--- main.mjs
+++ main.mjs
@@ -11,6 +11,20 @@ class ChessBoard {
     }
     return board;
   }
+
+  knightMoves(startingPosition, endPosition) {
+    // takes a starting position e.g. [3,3] and a end position e.g. [0,0] and finds the shortest path to reach it.
+    // e.g. [[3,3],[2,1],[0,0]] 
+    // function to calculate nextPossibleMoves(startingPosition), that returns an adjacency list
+  }
+}
+
+class Square {
+  constructor (coordinates) {
+    this.currentPosition = coordinates; 
+    this.parent = null;
+    this.children = [];
+  }
 }
 
 const test = new ChessBoard(8);
